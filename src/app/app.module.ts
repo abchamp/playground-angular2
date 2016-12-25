781244d20a52e881968447079d303d57c6187a9f
Champ Katika <info.bughacking@gmail.com>
2016-12-25 16:15:13 +0700

    add home and shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+// Component 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { 
+    HeaderComponent,
+    SharedModule 
+} from './shared';
+
+//routing
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // for Component 
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
-  imports: [
+  imports: [ // for Module
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule // it's Routing 
   ],
-  providers: [],
+  providers: [], // for Serveice
   bootstrap: [AppComponent]
 })
 export class AppModule { }
